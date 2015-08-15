52cb113e455db31a20ca84b11ff727eb79f2b786
kevin.zhu <kevin.zhu@alo7.com>
2015-08-15 12:31:24 +0800

    remove some alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -13,19 +13,18 @@ elif [[ $unamestr == 'Darwin' ]]; then
 fi
 
 # YADR support
-alias yav='yadr vim-add-plugin'
-alias ydv='yadr vim-delete-plugin'
-alias ylv='yadr vim-list-plugin'
-alias yup='yadr update-plugins'
-alias yip='yadr init-plugins'
+#alias yav='yadr vim-add-plugin'
+#alias ydv='yadr vim-delete-plugin'
+#alias yup='yadr update-plugins'
+#alias yip='yadr init-plugins'
 
 # PS
-alias psa="ps aux"
-alias psg="ps aux | grep "
-alias psr='ps aux | grep ruby'
+#alias psa="ps aux"
+#alias psg="ps aux | grep "
+#alias psr='ps aux | grep ruby'
 
 # Moving around
-alias cdb='cd -'
+#alias cdb='cd -'
 alias cls='clear;ls'
 
 # Show human friendly numbers and colors
@@ -41,7 +40,7 @@ elif [[ $platform == 'darwin' ]]; then
 fi
 
 # show me files matching "ls grep"
-alias lsg='ll | grep'
+#alias lsg='ll | grep'
 
 # Alias Editing
 alias ae='vim $yadr/zsh/aliases.zsh' #alias edit
@@ -58,137 +57,137 @@ fi
 alias :q='exit'
 
 # vimrc editing
-alias ve='vim ~/.vimrc'
+#alias ve='vim ~/.vimrc'
 
 # zsh profile editing
-alias ze='vim ~/.zshrc'
-alias zr='source ~/.zshrc'
+#alias ze='vim ~/.zshrc'
+#alias zr='source ~/.zshrc'
 
 # Git Aliases
-alias gs='git status'
-alias gstsh='git stash'
-alias gst='git stash'
-alias gsp='git stash pop'
-alias gsa='git stash apply'
-alias gsh='git show'
-alias gshw='git show'
-alias gshow='git show'
-alias gi='vim .gitignore'
-alias gcm='git ci -m'
-alias gcim='git ci -m'
-alias gci='git ci'
-alias gco='git co'
-alias gcp='git cp'
-alias ga='git add -A'
-alias guns='git unstage'
-alias gunc='git uncommit'
-alias gm='git merge'
-alias gms='git merge --squash'
-alias gam='git amend --reset-author'
-alias grv='git remote -v'
-alias grr='git remote rm'
-alias grad='git remote add'
-alias gr='git rebase'
-alias gra='git rebase --abort'
-alias ggrc='git rebase --continue'
-alias gbi='git rebase --interactive'
-alias gl='git l'
-alias glg='git l'
-alias glog='git l'
-alias co='git co'
-alias gf='git fetch'
-alias gfch='git fetch'
-alias gd='git diff'
-alias gb='git b'
-alias gbd='git b -D -w'
+#alias gs='git status'
+#alias gstsh='git stash'
+#alias gst='git stash'
+#alias gsp='git stash pop'
+#alias gsa='git stash apply'
+#alias gsh='git show'
+#alias gshw='git show'
+#alias gshow='git show'
+#alias gi='vim .gitignore'
+#alias gcm='git ci -m'
+#alias gcim='git ci -m'
+#alias gci='git ci'
+#alias gco='git co'
+#alias gcp='git cp'
+#alias ga='git add -A'
+#alias guns='git unstage'
+#alias gunc='git uncommit'
+#alias gm='git merge'
+#alias gms='git merge --squash'
+#alias gam='git amend --reset-author'
+#alias grv='git remote -v'
+#alias grr='git remote rm'
+#alias grad='git remote add'
+#alias gr='git rebase'
+#alias gra='git rebase --abort'
+#alias ggrc='git rebase --continue'
+#alias gbi='git rebase --interactive'
+#alias gl='git l'
+#alias glg='git l'
+#alias glog='git l'
+#alias co='git co'
+#alias gf='git fetch'
+#alias gfch='git fetch'
+#alias gd='git diff'
+#alias gb='git b'
+#alias gbd='git b -D -w'
 # Staged and cached are the same thing
-alias gdc='git diff --cached -w'
-alias gds='git diff --staged -w'
-alias gpub='grb publish'
-alias gtr='grb track'
-alias gpl='git pull'
-alias gplr='git pull --rebase'
-alias gps='git push'
-alias gpsh='git push -u origin `git rev-parse --abbrev-ref HEAD`'
-alias gnb='git nb' # new branch aka checkout -b
-alias grs='git reset'
-alias grsh='git reset --hard'
-alias gcln='git clean'
-alias gclndf='git clean -df'
-alias gclndfx='git clean -dfx'
-alias gsm='git submodule'
-alias gsmi='git submodule init'
-alias gsmu='git submodule update'
-alias gt='git t'
-alias gbg='git bisect good'
-alias gbb='git bisect bad'
+#alias gdc='git diff --cached -w'
+#alias gds='git diff --staged -w'
+#alias gpub='grb publish'
+#alias gtr='grb track'
+#alias gpl='git pull'
+#alias gplr='git pull --rebase'
+#alias gps='git push'
+#alias gpsh='git push -u origin `git rev-parse --abbrev-ref HEAD`'
+#alias gnb='git nb' # new branch aka checkout -b
+#alias grs='git reset'
+#alias grsh='git reset --hard'
+#alias gcln='git clean'
+#alias gclndf='git clean -df'
+#alias gclndfx='git clean -dfx'
+#alias gsm='git submodule'
+#alias gsmi='git submodule init'
+#alias gsmu='git submodule update'
+#alias gt='git t'
+#alias gbg='git bisect good'
+#alias gbb='git bisect bad'
 
 # Common shell functions
 alias less='less -r'
 alias tf='tail -f'
-alias l='less'
-alias lh='ls -alt | head' # see the last modified files
-alias screen='TERM=screen screen'
+#alias l='less'
+#alias lh='ls -alt | head' # see the last modified files
+#alias screen='TERM=screen screen'
 alias cl='clear'
 
 # Zippin
-alias gz='tar -zcvf'
+#alias gz='tar -zcvf'
 
 # Ruby
-alias c='rails c' # Rails 3
-alias co='script/console --irb=pry' # Rails 2
-alias ts='thin start'
-alias ms='mongrel_rails start'
-alias tfdl='tail -f log/development.log'
-alias tftl='tail -f log/test.log'
+#alias c='rails c' # Rails 3
+#alias co='script/console --irb=pry' # Rails 2
+#alias ts='thin start'
+#alias ms='mongrel_rails start'
+#alias tfdl='tail -f log/development.log'
+#alias tftl='tail -f log/test.log'
 
-alias ka9='killall -9'
-alias k9='kill -9'
+#alias ka9='killall -9'
+#alias k9='kill -9'
 
 # Gem install
-alias sgi='sudo gem install --no-ri --no-rdoc'
+#alias sgi='sudo gem install --no-ri --no-rdoc'
 
 # TODOS
 # This uses NValt (NotationalVelocity alt fork) - http://brettterpstra.com/project/nvalt/
 # to find the note called 'todo'
-alias todo='open nvalt://find/todo'
+#alias todo='open nvalt://find/todo'
 
 # Forward port 80 to 3000
-alias portforward='sudo ipfw add 1000 forward 127.0.0.1,3000 ip from any to any 80 in'
+#alias portforward='sudo ipfw add 1000 forward 127.0.0.1,3000 ip from any to any 80 in'
 
-alias rdm='rake db:migrate'
-alias rdmr='rake db:migrate:redo'
+#alias rdm='rake db:migrate'
+#alias rdmr='rake db:migrate:redo'
 
 # Zeus
-alias zs='zeus server'
-alias zc='zeus console'
+#alias zs='zeus server'
+#alias zc='zeus console'
 
 # Rspec
-alias rs='rspec spec'
-alias sr='spring rspec'
-alias src='spring rails c'
-alias srgm='spring rails g migration'
-alias srdm='spring rake db:migrate'
-alias srdt='spring rake db:migrate'
-alias srdmt='spring rake db:migrate db:test:prepare'
+#alias rs='rspec spec'
+#alias sr='spring rspec'
+#alias src='spring rails c'
+#alias srgm='spring rails g migration'
+#alias srdm='spring rake db:migrate'
+#alias srdt='spring rake db:migrate'
+#alias srdmt='spring rake db:migrate db:test:prepare'
 
 # Sprintly - https://github.com/nextbigsoundinc/Sprintly-GitHub
-alias sp='sprintly'
+#alias sp='sprintly'
 # spb = sprintly branch - create a branch automatically based on the bug you're working on
-alias spb="git checkout -b \`sp | tail -2 | grep '#' | sed 's/^ //' | sed 's/[^A-Za-z0-9 ]//g' | sed 's/ /-/g' | cut -d"-" -f1,2,3,4,5\`"
+#alias spb="git checkout -b \`sp | tail -2 | grep '#' | sed 's/^ //' | sed 's/[^A-Za-z0-9 ]//g' | sed 's/ /-/g' | cut -d"-" -f1,2,3,4,5\`"
 
-alias hpr='hub pull-request'
-alias grb='git recent-branches'
+#alias hpr='hub pull-request'
+#alias grb='git recent-branches'
 
 # Finder
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
 
-alias dbtp='spring rake db:test:prepare'
-alias dbm='spring rake db:migrate'
-alias dbmr='spring rake db:migrate:redo'
-alias dbmd='spring rake db:migrate:down'
-alias dbmu='spring rake db:migrate:up'
+#alias dbtp='spring rake db:test:prepare'
+#alias dbm='spring rake db:migrate'
+#alias dbmr='spring rake db:migrate:redo'
+#alias dbmd='spring rake db:migrate:down'
+#alias dbmu='spring rake db:migrate:up'
 
 
 #ls tree view
@@ -197,15 +196,15 @@ alias lr='ls -R | grep ":$" | sed -e "s/:$//" -e "s/[^-][^\/]*\//--/g" -e "s/^/ 
 alias l.='ls -dl .*'
 alias netlisteners='lsof -i -P | grep LISTEN'
 alias internetip='curl ifconfig.me'
-alias wotgobblemem='LC_ALL=C sudo ps -o time,ppid,pid,nice,pcpu,pmem,user,comm -A | sort -n -k 6 | tail -15'
-mcd() { mkdir -p "$1" && cd "$1" }
-t() {tail -f $1 | perl -pe "s/$2/\e[1;31;43m$&\e[0m/g"}
-sssh (){ ssh -t "$1" 'tmux attach -t leon || tmux new -s leon || screen -d -RR -S leon'}
+#alias wotgobblemem='LC_ALL=C sudo ps -o time,ppid,pid,nice,pcpu,pmem,user,comm -A | sort -n -k 6 | tail -15'
+#mcd() { mkdir -p "$1" && cd "$1" }
+#t() {tail -f $1 | perl -pe "s/$2/\e[1;31;43m$&\e[0m/g"}
+#sssh (){ ssh -t "$1" 'tmux attach -t leon || tmux new -s leon || screen -d -RR -S leon'}
 authme() { ssh "$1" 'mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys' < ~/.ssh/id_rsa.pub }
-servedir(){ python -m SimpleHTTPServer $1 }
-workdone(){
-    default="1"
-    git log --committer=leon --pretty=format:"%Cgreen%ar (%h)%n%Creset> %s %b%n" --since="${1:-$default} day ago" --no-merges
-}
+#servedir(){ python -m SimpleHTTPServer $1 }
+#workdone(){
+#    default="1"
+#    git log --committer=leon --pretty=format:"%Cgreen%ar (%h)%n%Creset> %s %b%n" --since="${1:-$default} day ago" --no-merges
+#}
 # Homebrew
-alias brewu='brew update && brew upgrade && brew cleanup && brew prune && brew doctor'
+#alias brewu='brew update && brew upgrade && brew cleanup && brew prune && brew doctor'
